b4fb19cf0390de4a094ac53c618e2917753e9d4e
Shahidc0de <shahidzada.cs@gmail.com>
2024-06-08 15:59:52 +0500

    security actions

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,19 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:techtrove_admin/provider/app_provider.dart';
 import 'package:techtrove_admin/screens/home_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
+  await dotenv.load();
   WidgetsFlutterBinding.ensureInitialized();
   try {
+    final api_key = dotenv.env["API_KEY"] ?? "";
     //firebase was returning a null value, so this helped to solve the problem;
     Platform.isAndroid
         ? await Firebase.initializeApp(
-            options: const FirebaseOptions(
-                apiKey: "AIzaSyCWa2oMnwVYsUapCby5pyZNmzY3_z-denY",
+            options: FirebaseOptions(
+                apiKey: api_key,
+                // "AIzaSyCWa2oMnwVYsUapCby5pyZNmzY3_z-denY",
                 appId: "1:973121984053:android:eb57c772b96d8a919b437c",
                 messagingSenderId: "973121984053",
                 projectId: "tech-trove-shop-3ea50"),
